838e8eae884bec298cfccc23b94fe58d28c0f3dd
ahmedsallam <ahmed.salam@rentcentric.com>
2022-08-14 21:06:04 +0200

    Initial Commit

---
+++ Shared/Words_PuzzleApp.swift
@@ -0,0 +1,20 @@
+//
+//  Words_PuzzleApp.swift
+//  Shared
+//
+//  Created by Ahmed Sallam on 14/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Words_PuzzleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
